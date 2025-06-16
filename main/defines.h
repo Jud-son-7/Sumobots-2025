adf089a77ab543f9fb481fac7d6ce30e9f6f4dc6
Tinodashe <z5636201@ad.unsw.edu.au>
2025-06-16 22:30:26 +1000

    initial code

---
+++ main/defines.h
@@ -0,0 +1,20 @@
+#include <Servo.h>
+#include <MP
+
+// TODO: allocate pins
+#define ULT_1_TRIG 
+#define ULT_1_ECHO
+#define ULT_2_TRIG 
+#define ULT_2_ECHO
+#define ULT_3_TRIG 
+#define ULT_3_ECHO
+#define RIGHT_WHEEL
+#define LEFT_WHEEL
+#define LF_FWD
+#define LF_BWD
+#define RT_FWD
+#define RT_BWD
+#define LIGHT
+
+
+
---
+++ main/main.ino
@@ -0,0 +1,12 @@
+void setup() {
+  double dist_1 = 0;
+  double dist_2 = 0;
+  double dist_3 = 0; 
+  double acc = 0;
+  double weight = 0; 
+  bool out_of_bounds = false;
+}
+
+void loop(){
+// TODO: Apply sumobit logic
+}
